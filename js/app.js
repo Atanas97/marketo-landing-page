b7c158c2a18041c35e79770a06ff883264ac3ad9
Atanas97 <atanas19.97@abv.bg>
2021-08-29 21:18:04 +0300

    finished website

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 const btn = document.getElementById('toggle-menu')
 const mobileNav = document.querySelector('.header-nav')
 const body = document.querySelector('body')
-console.log(body)
+const menu = document.getElementById('menu-list')
+const menuEls = menu.querySelectorAll('li')
+console.log(menuEls)
 btn.addEventListener('click', () => {
     mobileNav.classList.toggle('active')
 
     mobileNav.classList.contains('active') ? btn.classList = 'fas fa-times' : btn.classList = 'fas fa-bars'
 })
+const hideMenu = () => {
+    mobileNav.classList.remove('active')
+}
+
+menuEls.forEach(list => {
+    list.addEventListener('click', hideMenu)
+});
+
 
 // body.addEventListener('click', (e) => {
 //     e.stopPropagation()
 //     mobileNav.classList.remove('active')
 //     btn.classList = 'fas fa-bars'
 // })
+
